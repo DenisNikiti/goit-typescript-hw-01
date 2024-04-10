fb7867a79738257349d3efacbea2615fee48b11d
DenisNikiti <den.hikitin@gmail.com>
2024-04-10 23:39:51 +0300

    7

---
+++ dist/concatenation.js
@@ -0,0 +1,3 @@
+export const concatenation = (value, message) => {
+    console.log(`${value} ${message}`);
+};
---
+++ src/concatenation.ts
@@ -0,0 +1,3 @@
+export const concatenation = (value: string, message: string): void => {
+  console.log(`${value} ${message}`);
+};
